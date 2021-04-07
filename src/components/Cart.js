1c784565e67202f89a32d1c6fe537400b0a1be07
Jorge Sisco <jorgesisco17@gmail.com>
2021-04-07 16:50:14 -0400

    Update Cart.js

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -13,6 +13,25 @@ export default class Cart extends Component {
             You have {cartItems.length} in the cart{' '}
           </div>
         )}
+        <div>
+          <div className='cart'>
+            <ul className='cart-items'>
+              {cartItems.map((item) => (
+                <li key={item._id}>
+                  <div>
+                    <img src={item.image} alt={item.title}></img>
+                  </div>
+                  <div>
+                    <div>{item.title}</div>
+                    <button onClick={() => this.props.removeFromCart(item)}>
+                      Remove
+                    </button>
+                  </div>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
       </Container>
     );
   }
@@ -20,7 +39,11 @@ export default class Cart extends Component {
 
 const Container = styled.div`
   .cart {
+    padding: 1rem;
+    margin: 1rem;
+    display: flex;
   }
   .cart-header {
+    border-bottom: 0.1rem #c0c0c0 solid;
   }
 `;
